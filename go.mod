36c58a6466b602ee258b2bdc0013db4007663a93
zetraison <alexse790@mail.ru>
2020-04-30 00:12:01 +0500

    Add DetectIntentText message handler

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module dialogflow-telegram-bot
 go 1.14
 
 require (
+	cloud.google.com/go v0.56.0
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	google.golang.org/genproto v0.0.0-20200428115010-c45acf45369a
 )
--- main.go
+++ main.go
@@ -1,17 +1,65 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"log"
 	"os"
 	"time"
 
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	dialogflow "cloud.google.com/go/dialogflow/apiv2"
+	dialogflowpb "google.golang.org/genproto/googleapis/cloud/dialogflow/v2"
+
+	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
+
+const (
+	ruLanguage   = "ru"
+	errorMessage = "Повторите, пожалуйста, вопрос."
+)
+
+func DetectIntentText(ctx context.Context, projectID, sessionID, text, languageCode string) (string, error) {
+	sessionClient, err := dialogflow.NewSessionsClient(ctx)
+	if err != nil {
+		return "", err
+	}
+	defer sessionClient.Close()
+
+	if projectID == "" || sessionID == "" {
+		return "", errors.New(fmt.Sprintf("Received empty project (%s) or session (%s)", projectID, sessionID))
+	}
+
+	sessionPath := fmt.Sprintf("projects/%s/agent/sessions/%s", projectID, sessionID)
+	textInput := dialogflowpb.TextInput{Text: text, LanguageCode: languageCode}
+	queryTextInput := dialogflowpb.QueryInput_Text{Text: &textInput}
+	queryInput := dialogflowpb.QueryInput{Input: &queryTextInput}
+	request := dialogflowpb.DetectIntentRequest{Session: sessionPath, QueryInput: &queryInput}
+
+	response, err := sessionClient.DetectIntent(ctx, &request)
+	if err != nil {
+		return "", err
+	}
+
+	queryResult := response.GetQueryResult()
+	fulfillmentText := queryResult.GetFulfillmentText()
+	return fulfillmentText, nil
+}
 
 func main() {
 	tgApiToken := os.Getenv("TELEGRAM_API_TOKEN")
 	if len(tgApiToken) == 0 {
 		panic("Telegram API token not set!")
+	}
+
+	projectID := os.Getenv("PROJECT_ID")
+	if len(projectID) == 0 {
+		panic("Project ID not set!")
+	}
+
+	sessionID := os.Getenv("SESSION_ID")
+	if len(sessionID) == 0 {
+		panic("Session ID not set!")
 	}
 
 	bot, err := tgbotapi.NewBotAPI(tgApiToken)
@@ -21,6 +69,8 @@ func main() {
 	bot.Debug = true
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	ctx, _ := context.WithCancel(context.Background())
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
@@ -39,7 +89,13 @@ func main() {
 
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		text, err := DetectIntentText(ctx, projectID, sessionID, update.Message.Text, ruLanguage)
+		if err != nil {
+			text = errorMessage
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
 		msg.ReplyToMessageID = update.Message.MessageID
 
 		bot.Send(msg)
